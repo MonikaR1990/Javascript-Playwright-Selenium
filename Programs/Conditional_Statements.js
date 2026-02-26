64ec0ab5034401089a1a2a4e90dd3253a1e98505
Monika R <monicaselas2010@gmail.com>
2026-02-26 14:15:09 +0530

    Feb 27

---
+++ Programs/Conditional_Statements.js
@@ -0,0 +1,61 @@
+//Control Flow Statements
+// 1. if 
+//2. if-else 
+//3. if-else-if
+//4. switch
+
+let mark = 90;
+
+if(mark>=50)
+{
+    console.log("Pass")
+}
+else
+{
+    console.log("Fail");
+}
+
+//if-else-if
+
+let score = 60;
+
+if(score>=90)
+{
+    console.log("Grade A");
+}
+else if(score>=75)
+{
+    console.log("Grade B")
+}
+else if(score>=65)
+{
+    console.log("Grade C");
+}
+else if(score>=50)
+{
+    console.log("Grade D")
+}
+else
+{
+    console.log("Fail")
+}
+
+let signal = "Yellow"
+
+if(signal === "red")
+{
+    console.log("Stop");
+}
+else if(signal === "Green")
+{
+    console.log("Go")
+}
+else if(signal === "Yellow")
+{
+    console.log("Ready")
+}
+else
+{
+    console.log("Signal not Working");
+}
+
--- Programs/Operators.js
+++ Programs/Operators.js
@@ -35,17 +35,106 @@ let p = 10;
 
 p += 5; // p = p+5;
 
-console.log(p);
+console.log(p); //15
 
 p -= 10; //p = p - 10
 
-console.log(p);
+console.log(p); //5
 
-p *= 2; //p = p * 2
+p *= 2; //p = 5 * 2
 
-console.log(p);
+console.log(p); 10
 
 p /= 2; //p = p / 2;
 console.log(p);
 
 
+let s = 20;
+
+s *= 2; s = 20 * 2
+
+console.log(s); //40
+
+
+//Comparison Operator (==, ===, !=, !==, <, >, >=, <=)
+
+console.log(5==5); 
+
+console.log(5=="5"); //== value only check
+
+console.log(5==="5"); // === Strict Equal  (value + data type check)
+
+console.log(5!=4);
+
+console.log(5!=="4")
+
+console.log(5>5); //false
+
+ console.log(5>=5); //true
+
+
+//Logical Operator (&&, ||)
+
+let age = 16;
+let hasVoterId = true;
+
+if(age>=18 && hasVoterId)
+{
+    console.log("Eligible for Vote")
+}
+else{
+    console.log("Not Eligible for Vote")
+}
+
+
+console.log(age>=18 && hasVoterId);
+
+console.log(age>=18 || hasVoterId);
+
+//Unary Operator (++, --)
+
+let m = 5;
+
+
+console.log(m++); //Post Increment //First Value print then increase 1
+
+console.log(m);
+
+let n = 7;
+
+console.log(++n); //Pre-Increment //
+
+let i = -7;
+
+console.log(i--); //7
+
+console.log(i); //6
+
+//Unary minus (-)
+
+console.log(-i);
+
+//Logical Not (!) //Boolean value reverse
+
+let isstatus = true
+console.log(!isstatus)
+
+//tenary operator
+
+let mark =  40;
+
+let result = mark >=50 ? "Pass" : "Fail";
+
+console.log(result);
+
+let color = "Green"
+
+
+console.log(color == "Green" ? "Go" : "Stop");
+
+
+//Type of Operator
+
+console.log(typeof color);
+console.log(typeof mark);
+console.log(typeof isstatus);
